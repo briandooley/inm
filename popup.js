3b6644a2515180db27e04b382cec0365b3b92ff2
Brian Dooley <brian.dooley@feedhenry.com>
2016-07-06 19:49:44 +0100

    working 1st go

--- popup.js
+++ popup.js
@@ -1,90 +1,51 @@
-// Copyright (c) 2014 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-/**
- * Get the current URL.
- *
- * @param {function(string)} callback - called when the URL of the current tab
- *   is found.
- */
 function getCurrentTabUrl(callback) {
-  // Query filter to be passed to chrome.tabs.query - see
-  // https://developer.chrome.com/extensions/tabs#method-query
   var queryInfo = {
     active: true,
     currentWindow: true
   };
 
   chrome.tabs.query(queryInfo, function(tabs) {
-    // chrome.tabs.query invokes the callback with a list of tabs that match the
-    // query. When the popup is opened, there is certainly a window and at least
-    // one tab, so we can safely assume that |tabs| is a non-empty array.
-    // A window can only have one active tab at a time, so the array consists of
-    // exactly one tab.
     var tab = tabs[0];
-
-    // A tab is a plain object that provides information about the tab.
-    // See https://developer.chrome.com/extensions/tabs#type-Tab
     var url = tab.url;
-
-    // tab.url is only available if the "activeTab" permission is declared.
-    // If you want to see the URL of other tabs (e.g. after removing active:true
-    // from |queryInfo|), then the "tabs" permission is required to see their
-    // "url" properties.
     console.assert(typeof url == 'string', 'tab.url should be a string');
 
     callback(url);
   });
-
-  // Most methods of the Chrome extension APIs are asynchronous. This means that
-  // you CANNOT do something like this:
-  //
-  // var url;
-  // chrome.tabs.query(queryInfo, function(tabs) {
-  //   url = tabs[0].url;
-  // });
-  // alert(url); // Shows "undefined", because chrome.tabs.query is async.
 }
 
-/**
- * @param {string} searchTerm - Search term for Google Image search.
- * @param {function(string,number,number)} callback - Called when an image has
- *   been found. The callback gets the URL, width and height of the image.
- * @param {function(string)} errorCallback - Called when the image is not found.
- *   The callback gets a string that describes the failure reason.
- */
-function getImageUrl(searchTerm, callback, errorCallback) {
-  // Google image search - 100 searches per day.
-  // https://developers.google.com/image-search/
-  console.log('searchTerm is ' + searchTerm);
-  var newTab = searchTerm.substring(0, searchTerm.search('#')) + 'api/v2/info/';
-  chrome.tabs.create( {'url': newTab, 'active': false});
+function makeTabs(baseUrl, callback, errorCallback) {
+  console.log('baseUrl is ' + baseUrl);
 
-  var searchUrl = 'https://support.us.feedhenry.com/api/v2/info/';
+  if (baseUrl.search('#') > 0) {
+    // Use # to verify that the url is a feedhenry/redhat one.  Doesn't work in some cases though.
+    var newTab = baseUrl.substring(0, baseUrl.search('#')) + 'api/v2/info/';
 
-  var x = new XMLHttpRequest();
-  x.open('GET', newTab);
-  // The Google image search API responds with JSON, so let Chrome parse it.
-  x.onload = function() {
-    // Parse and process the response from Google Image Search.
-    var response = JSON.parse(x.response);
+    //renderStatus('Opening Info Tab');
 
-    console.log(response.solution.core.id);
+    var req = new XMLHttpRequest();
+    req.open('GET', newTab);
 
-    chrome.tabs.create( {'url': response.solution.core.id + '-mgt1.feedhenry.net:8811', 'active': false});
-    chrome.tabs.create( {'url': response.solution.core.id + '-mgt1.feedhenry.net:8810/munin', 'active': false});
+    req.onload = function() {
+      // Parse and process the response.
+      var response = JSON.parse(req.response);
 
-    if (!response || !response.responseData || !response.responseData.results ||
-        response.responseData.results.length === 0) {
-      errorCallback('No response from Google Image search!');
-      return;
+      chrome.tabs.create({'url': newTab, 'active': true});
+      chrome.tabs.create( {'url': response.solution.core.id + '-mgt1.feedhenry.net:8811', 'active': false});
+      chrome.tabs.create( {'url': response.solution.core.id + '-mgt1.feedhenry.net:8810/munin', 'active': false});
+      window.close();
+  /*
+      if (!response || !response.responseData || !response.responseData.results ||
+          response.responseData.results.length === 0) {
+        errorCallback('No response from Google Image search!');
+        return;
+      }
+  */
     }
   };
-  x.onerror = function() {
+  req.onerror = function() {
     errorCallback('Network error.');
   };
-  x.send();
+  req.send();
 }
 
 function renderStatus(statusText) {
@@ -94,25 +55,9 @@ function renderStatus(statusText) {
 document.addEventListener('DOMContentLoaded', function() {
   console.log('DOM content has loaded');
   getCurrentTabUrl(function(url) {
-    // Put the image URL in Google search.
-    renderStatus('Searching ' + url);
-
-    getImageUrl(url, function(imageUrl, width, height) {
-
-      renderStatus('Search term: ' + url + '\n' +
-          'Google image search result: ' + imageUrl);
-      var imageResult = document.getElementById('image-result');
-      // Explicitly set the width/height to minimize the number of reflows. For
-      // a single image, this does not matter, but if you're going to embed
-      // multiple external images in your page, then the absence of width/height
-      // attributes causes the popup to resize multiple times.
-      imageResult.width = width;
-      imageResult.height = height;
-      imageResult.src = imageUrl;
-      imageResult.hidden = false;
-
+    makeTabs(url, function(imageUrl, width, height) {
     }, function(errorMessage) {
-      renderStatus('Cannot display image. ' + errorMessage);
-    });
+      //renderStatus('Cannot display image. ' + errorMessage);
+    })
   });
 });

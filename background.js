e95b3012d3b6574b3dacdf670f02a218b61dfcb8
Brian Dooley <brian.dooley@feedhenry.com>
2016-07-21 14:22:47 +0100

    added mbaas pages

--- background.js
+++ background.js
@@ -36,9 +36,19 @@ chrome.pageAction.onClicked.addListener(function(tab) {
     // Parse and process the response.
     var response = JSON.parse(req.response);
 
+    // Create tab for info page.
     chrome.tabs.create({'url': newTab, 'active': true});
+
+    // Create tabs for core Nagios and Munin pages.
     chrome.tabs.create( {'url': response.solution.core.id + '-mgt1.feedhenry.net:8811', 'active': false});
     chrome.tabs.create( {'url': response.solution.core.id + '-mgt1.feedhenry.net:8810/munin', 'active': false});
+
+    // Each info page has multiple mbaas ids.  Create a Nagios and Munin page for each one.
+    for (var i = 0; i < response.solution.mbaas.length; i++) {
+      chrome.tabs.create( {'url': response.solution.mbaas[i].id + '-mgt1.feedhenry.net:8811', 'active': false});
+      chrome.tabs.create( {'url': response.solution.mbaas[i].id + '-mgt1.feedhenry.net:8810/munin', 'active': false});
+    }
+
     window.close();
   }
   req.onerror = function() {
